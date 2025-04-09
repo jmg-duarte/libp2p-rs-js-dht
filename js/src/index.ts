38c41e22b3533b8156542937c638a480616fda54
dozyio <github@dozy.io>
2025-04-09 17:28:15 +0100

    working

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,32 +1,26 @@
 import { noise } from "@chainsafe/libp2p-noise"
 import { yamux } from "@chainsafe/libp2p-yamux"
-import { autoNAT } from "@libp2p/autonat"
 import { bootstrap } from "@libp2p/bootstrap"
+import { ping } from "@libp2p/ping"
 import { identify, identifyPush } from "@libp2p/identify"
-import { kadDHT } from "@libp2p/kad-dht"
+import { kadDHT, removePublicAddressesMapper } from "@libp2p/kad-dht"
 import { peerIdFromString } from "@libp2p/peer-id"
 import { webSockets } from "@libp2p/websockets"
-import { multiaddr } from "@multiformats/multiaddr"
 import { createLibp2p } from "libp2p"
-import { perform_query, setup_logging } from "kad-query"
-import { tcp } from "@libp2p/tcp"
-
 
 async function createNode(bootnodes: string[]) {
     return await createLibp2p(
         {
             addresses: {
                 listen: [
-                    "/ip4/0.0.0.0/tcp/0",
                     "/ip4/0.0.0.0/tcp/0/ws"
                 ]
             },
-            transports: [tcp(), webSockets()],
+            transports: [webSockets()],
             connectionEncrypters: [noise()],
             streamMuxers: [yamux()],
             peerDiscovery: [
                 bootstrap({
-                    // Add a list of bootnodes to ensure they're around
                     list: bootnodes,
                 }),
             ],
@@ -34,42 +28,39 @@ async function createNode(bootnodes: string[]) {
                 identify: identify(),
                 identifyPush: identifyPush(),
                 dht: kadDHT({
-                    // This is supposed to be an ephemeral client
-                    clientMode: false,
-                    // Required for local testing, by default, js-libp2p will remove them,
-                    // even though this isn't documented anywhere...
-                    peerInfoMapper: (peer) => peer
+                    peerInfoMapper: removePublicAddressesMapper,
+                    clientMode: true,
                 }),
-                autonat: autoNAT(),
+                ping: ping()
             },
-            connectionMonitor: {
-                enabled: false
+            connectionGater: {
+              denyDialMultiaddr: () => false
             },
         },
     )
 }
 
 async function main() {
-    const [_node, _script, bootnodesArg, query] = process.argv
-    if (!bootnodesArg) {
+    const [_node, _script, _, bootnodes, query] = process.argv
+    if (!bootnodes) {
         throw new Error("Missing \"bootnodes\"")
     }
     if (!query) {
         throw new Error("Missing \"query\"")
     }
-    const bootnodes = bootnodesArg.split(",")
 
+    console.log('bootnodes', bootnodes)
+    console.log('query', query)
 
+    const node = await createNode(bootnodes.split(","))
+    // console.log('node', node)
+    // node.addEventListener("self:peer:update", evt => {
+    //   console.log('self:peer:update', evt.detail)
+    //   console.log(node.getMultiaddrs())
+    // })
 
-    const node = await createNode(bootnodes)
     const peerId = peerIdFromString(query)
-
-    for (const bootnode of bootnodes) {
-        console.log(`Dialing ${bootnode}`)
-        const _ = await node.dial(multiaddr(bootnode))
-    }
-
-    console.log(await node.contentRouting.get(peerId.toMultihash().bytes, { signal: AbortSignal.timeout(5000) }))
+    console.log("contentRouting get", await node.contentRouting.get(peerId.toMultihash().bytes))
 }
 
 main()
